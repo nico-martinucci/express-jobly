b32a424f02004907dc24fb4230ed09480a2bf086
Nico Martinucci <domenico.martinucci@gmail.com>
2023-01-12 10:57:45 -0800

    add tests for admin/current user middleware

--- middleware/auth.test.js
+++ middleware/auth.test.js
@@ -5,6 +5,8 @@ const { UnauthorizedError } = require("../expressError");
 const {
   authenticateJWT,
   ensureLoggedIn,
+  ensureIsAdmin,
+  ensureIsAdminOrCurrentUser
 } = require("./auth");
 
 
@@ -21,6 +23,7 @@ describe("authenticateJWT", function () {
     const req = { headers: { authorization: `Bearer ${testJwt}` } };
     const res = { locals: {} };
     authenticateJWT(req, res, next);
+
     expect(res.locals).toEqual({
       user: {
         iat: expect.any(Number),
@@ -34,6 +37,7 @@ describe("authenticateJWT", function () {
     const req = {};
     const res = { locals: {} };
     authenticateJWT(req, res, next);
+
     expect(res.locals).toEqual({});
   });
 
@@ -41,6 +45,7 @@ describe("authenticateJWT", function () {
     const req = { headers: { authorization: `Bearer ${badJwt}` } };
     const res = { locals: {} };
     authenticateJWT(req, res, next);
+
     expect(res.locals).toEqual({});
   });
 });
@@ -56,6 +61,66 @@ describe("ensureLoggedIn", function () {
   test("unauth if no login", function () {
     const req = {};
     const res = { locals: {} };
+
     expect(() => ensureLoggedIn(req, res, next)).toThrowError();
+  });
+});
+
+describe("ensureIsAdmin", function () {
+  test("works", function () {
+    const req = {};
+    const res = { locals: { user: { isAdmin: true } } };
+
+    ensureIsAdmin(req, res, next);
+  });
+
+  test("unauth if not admin", function () {
+    const req = {};
+    const res = { locals: { user: { isAdmin: false } } };
+
+    expect(() => ensureIsAdmin(req, res, next)).toThrowError();
+  });
+  
+  test("unauth if not logged in", function () {
+    const req = {};
+    const res = { locals: {} };
+
+    expect(() => ensureIsAdmin(req, res, next)).toThrowError();
+  });
+});
+
+describe("ensureIsAdminOrCurrentUser", function () {
+  test("works for admin", function () {
+    const req = { params: { username: "u1" } };
+    const res = { locals: { user: { isAdmin: true } } };
+
+    ensureIsAdminOrCurrentUser(req, res, next);
+  });
+  
+  test("works for current user", function () {
+    const req = { params: { username: "u2" } };
+    const res = { locals: { user: { 
+      isAdmin: false,
+      username: "u2" 
+    } } };
+
+    ensureIsAdminOrCurrentUser(req, res, next);
+  });
+
+  test("unauth if not admin or current user", function () {
+    const req = { params: { username: "u2" } };
+    const res = { locals: { user: { 
+      isAdmin: false,
+      username: "u3" 
+    } } };
+
+    expect(() => ensureIsAdminOrCurrentUser(req, res, next)).toThrowError();
+  });
+  
+  test("unauth if not logged in", function () {
+    const req = {};
+    const res = { locals: {} };
+
+    expect(() => ensureIsAdminOrCurrentUser(req, res, next)).toThrowError();
   });
 });
